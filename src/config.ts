7869165247cc995707ca59ffe287a9acf92bfb08
Nicolás Herrera <nicolas.herrera@cauri.io>
2023-12-02 11:19:09 -0300

    feat: config por env vars, con defaults de staging para desarrollo local

---
+++ src/config.ts
@@ -0,0 +1,34 @@
+function required(name: string, fallback?: string): string {
+  const value = process.env[name] ?? fallback;
+  if (value === undefined) {
+    throw new Error(`missing required env var ${name}`);
+  }
+  return value;
+}
+
+export const config = {
+  serviceName: process.env.SERVICE_NAME ?? 'kyc-service',
+  env: process.env.ENV ?? 'staging',
+  port: Number(process.env.PORT ?? 8080),
+  logLevel: process.env.LOG_LEVEL ?? 'info',
+
+  keycloakIssuer: required('KEYCLOAK_ISSUER', 'http://keycloak.platform.svc:8080/realms/cauri-staging'),
+  keycloakClientId: process.env.KEYCLOAK_CLIENT_ID ?? 'kyc-service',
+  keycloakClientSecret: process.env.KEYCLOAK_CLIENT_SECRET ?? '',
+
+  awsEndpointUrl: required('AWS_ENDPOINT_URL', 'http://localstack.platform.svc:4566'),
+  awsRegion: process.env.AWS_REGION ?? 'us-east-1',
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID ?? 'test',
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? 'test',
+
+  s3BucketPrefix: process.env.S3_BUCKET_PREFIX ?? 'cauri-staging-',
+  sqsQueuePrefix: process.env.SQS_QUEUE_PREFIX ?? 'staging-',
+
+  veridocUrl: required('VERIDOC_URL', 'http://sandbox.veridoc-id.com'),
+  veridocApiKey: process.env.VERIDOC_API_KEY ?? '',
+
+  ledgerCoreUrl: process.env.LEDGER_CORE_URL ?? 'http://ledger-core:8080',
+};
+
+export const kycDocumentsBucket = `${config.s3BucketPrefix}kyc-documents`;
+export const kycEventsQueue = `${config.sqsQueuePrefix}kyc-events`;
